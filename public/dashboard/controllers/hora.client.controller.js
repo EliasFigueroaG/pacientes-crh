d94f555bd308b59754828a83c120c856601ad379
fdxshadow <fran.peagonza@gmail.com>
2017-09-07 11:46:58 -0300

    Inicio desarrollo dashboard

---
+++ public/dashboard/controllers/hora.client.controller.js
@@ -0,0 +1,13 @@
+angular.module('dashboard').controller('Hora',['$scope',function ($scope) {
+	$scope.pacientes = [
+		{rut:'18988430',
+		 telefono:'d'	
+			},
+		{rut:'18988710',
+		 telefono:'a'},
+		{rut:'18229444',
+		 telefono:'b'},
+		{rut:'10813411',
+		telefono:'c'}
+	]
+}])
